92c386679150f554e5051b3e5c7b9254723ed325
CDub <42496675+chriswoodbury@users.noreply.github.com>
2023-02-09 15:57:53 -0500

    Update MainPage.xaml.cs

--- MauiCode/MainPage.xaml.cs
+++ MauiCode/MainPage.xaml.cs
@@ -21,7 +21,7 @@ public partial class MainPage : ContentPage
 				new Label { Text = "Please log in", FontSize = 30, TextColor = Color.FromRgb(255, 255, 100), Margin = new Thickness(0, 30, 0, 0) },
 				new Label { Text = "Username", TextColor = Color.FromArgb("bfff00"), Margin = new Thickness(0, 20, 0, 0) },
 				new Entry { TextColor = Color.FromRgb(255, 255, 255), FontSize = 20, },
-                new Label { Text = "Password", TextColor = Color.FromArgb("bfff00"), Margin = new Thickness(0, 20, 0, 0) },
+                                new Label { Text = "Password", TextColor = Color.FromArgb("bfff00"), Margin = new Thickness(0, 20, 0, 0) },
 				new Entry { IsPassword = true, TextColor = Color.FromRgb(255, 255, 100), FontSize = 20 }
             }
 		};
